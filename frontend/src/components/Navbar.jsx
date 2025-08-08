21f2f1cb9a33a62cdc8792c76119cf91a8a6b4f4
Nandani Kirtani <160708805+Nandanikirtani@users.noreply.github.com>
2025-08-08 13:35:10 +0530

    Added signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,8 @@ export default function Navbar() {
   return (
     <div>
       <h1 className='bg-blue-500'>Navbar</h1>
+      <h1 className='bg-blue-500'>Signup</h1>
+      <h1 className='bg-blue-500'>Signin</h1>
     </div>
   )
 }
