28ab7289af83d3b06c98c3baed626559b8bafca9
Nandani Kirtani <160708805+Nandanikirtani@users.noreply.github.com>
2025-08-08 13:50:38 +0530

    Added signin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <h1 className='bg-blue-500'>Navbar</h1>
       <h1 className='bg-blue-500'>Signup</h1>
-      <h1 className='bg-blue-500'>Signin</h1>
+      {/* <h1 className='bg-blue-500'>Signin</h1> */}
     </div>
   )
 }
