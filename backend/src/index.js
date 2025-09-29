177ef3c93ad88cebb1e8d7704771172b160fb09c
Jashank06 <jaykumar0305@gmail.com>
2025-09-29 23:25:59 +0530

    Production

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,6 +44,10 @@ app.use(errorHandler);
 
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`🚀 Server running on http://localhost:${PORT}`)
+// );
+
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`🚀 Server running on http://0.0.0.0:${PORT}`)
 );
